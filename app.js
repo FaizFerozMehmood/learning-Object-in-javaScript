5b6172639fb7e17cd7f98d0b9bb264cb0384abf2
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-05-28 05:59:30 +0500

    dirst-commit

---
+++ app.js
@@ -0,0 +1,99 @@
+var  info ={
+    userName : "faiz",
+    id :12344,
+    Dobp :"1/2/2223",
+
+}
+info.grd = "c"
+info.dob = new Date("04-19-1997").getMonth()
+console.log("fullyears",dob);
+
+// console.log(delete info.age);
+// console.log(delete info.Dob);
+
+console.log(info)
+var userInfo = {
+    name : "faiz",
+    father : "m.ismail",
+
+}
+userInfo.DOB = new Date("1/5/2003").getFullYear()
+console.log(userInfo);
+
+var checking ={
+dateOfBirth : new Date("1-5-2003"),
+
+
+}
+checking.calculateage =function (){
+    console.log("this dateofbirth",this.dateOfBirth);
+}
+console.log(checking);
+ userInfo ={
+    age: new Date("1-5-2003").getFullYear()
+ }
+ userInfo.dob =new Date().getFullYear()//2024
+ userInfo.calculate = function(){
+var getAge = userInfo.age
+var result = userInfo.dob
+return result-getAge
+ }
+
+var myInfo = {
+    name :"faiz",
+    address :"karachi",
+    DOB : new Date("1-5-2003").getFullYear(),
+currentYear : new Date().getFullYear(),
+
+}
+myInfo.calculate = function(){
+ var result =this.currentYear-this.DOB
+ return result
+}
+console.log("myinfo",myInfo,"calculate",myInfo.calculate());
+
+var myInfo = {
+    name: "faiz",
+    address: "karachi",
+    DOB: new Date("2003-01-05"), // Complete date of birth
+    currentYear: new Date().getFullYear(),
+};
+
+myInfo.calculate = function () {
+    var result = this.currentYear - this.DOB.getFullYear();
+    return result;
+};
+
+console.log("bb",myInfo.calculate());
+
+
+var objt = {
+name:"faiz"
+}
+objt.calculateYear = function(){
+    var MYBOD= new Date("01-05-2003").getMonth()
+    console.log(MYBOD);
+    
+    var currentYear = new Date().getMonth()
+    var calculate = MYBOD - currentYear
+    return calculate
+}
+console.log(objt.calculateYear());
+
+var userInfo= {
+    name : "faiz",
+   
+}
+userInfo.show =function(){
+    var num1 = true;
+    var num12 = !false;
+    var result= num1 ==num12
+    return result
+}
+console.log(userInfo.show());
+
+
+
+
+
+
