db080398ffc339347c786bdc1c11a29bd5445ab6
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-05-29 01:21:51 +0500

    addition

--- app.js
+++ app.js
@@ -1,99 +1,115 @@
-var  info ={
-    userName : "faiz",
-    id :12344,
-    Dobp :"1/2/2223",
+// var  info ={
+//     userName : "faiz",
+//     id :12344,
+//     Dobp :"1/2/2223",
 
-}
-info.grd = "c"
-info.dob = new Date("04-19-1997").getMonth()
-console.log("fullyears",dob);
+// }
+// info.grd = "c"
+// info.dob = new Date("04-19-1997").getMonth()
+// console.log("fullyears",dob);
 
-// console.log(delete info.age);
-// console.log(delete info.Dob);
+// // console.log(delete info.age);
+// // console.log(delete info.Dob);
 
-console.log(info)
-var userInfo = {
-    name : "faiz",
-    father : "m.ismail",
+// console.log(info)
+// var userInfo = {
+//     name : "faiz",
+//     father : "m.ismail",
 
-}
-userInfo.DOB = new Date("1/5/2003").getFullYear()
-console.log(userInfo);
+// }
+// userInfo.DOB = new Date("1/5/2003").getFullYear()
+// console.log(userInfo);
 
-var checking ={
-dateOfBirth : new Date("1-5-2003"),
-
-
-}
-checking.calculateage =function (){
-    console.log("this dateofbirth",this.dateOfBirth);
-}
-console.log(checking);
- userInfo ={
-    age: new Date("1-5-2003").getFullYear()
- }
- userInfo.dob =new Date().getFullYear()//2024
- userInfo.calculate = function(){
-var getAge = userInfo.age
-var result = userInfo.dob
-return result-getAge
- }
-
-var myInfo = {
-    name :"faiz",
-    address :"karachi",
-    DOB : new Date("1-5-2003").getFullYear(),
-currentYear : new Date().getFullYear(),
-
-}
-myInfo.calculate = function(){
- var result =this.currentYear-this.DOB
- return result
-}
-console.log("myinfo",myInfo,"calculate",myInfo.calculate());
-
-var myInfo = {
-    name: "faiz",
-    address: "karachi",
-    DOB: new Date("2003-01-05"), // Complete date of birth
-    currentYear: new Date().getFullYear(),
-};
-
-myInfo.calculate = function () {
-    var result = this.currentYear - this.DOB.getFullYear();
-    return result;
-};
-
-console.log("bb",myInfo.calculate());
+// var checking ={
+// dateOfBirth : new Date("1-5-2003"),
 
 
-var objt = {
-name:"faiz"
-}
-objt.calculateYear = function(){
-    var MYBOD= new Date("01-05-2003").getMonth()
-    console.log(MYBOD);
+// }
+// checking.calculateage =function (){
+//     console.log("this dateofbirth",this.dateOfBirth);
+// }
+// console.log(checking);
+//  userInfo ={
+//     age: new Date("1-5-2003").getFullYear()
+//  }
+//  userInfo.dob =new Date().getFullYear()//2024
+//  userInfo.calculate = function(){
+// var getAge = userInfo.age
+// var result = userInfo.dob
+// return result-getAge
+//  }
+
+// var myInfo = {
+//     name :"faiz",
+//     address :"karachi",
+//     DOB : new Date("1-5-2003").getFullYear(),
+// currentYear : new Date().getFullYear(),
+
+// }
+// myInfo.calculate = function(){
+//  var result =this.currentYear-this.DOB
+//  return result
+// }
+// console.log("myinfo",myInfo,"calculate",myInfo.calculate());
+
+// var myInfo = {
+//     name: "faiz",
+//     address: "karachi",
+//     DOB: new Date("2003-01-05"), // Complete date of birth
+//     currentYear: new Date().getFullYear(),
+// };
+
+// myInfo.calculate = function () {
+//     var result = this.currentYear - this.DOB.getFullYear();
+//     return result;
+// };
+
+// console.log("bb",myInfo.calculate());
+
+
+// var objt = {
+// name:"faiz"
+// }
+// objt.calculateYear = function(){
+//     var MYBOD= new Date("01-05-2003").getMonth()
+//     console.log(MYBOD);
     
-    var currentYear = new Date().getMonth()
-    var calculate = MYBOD - currentYear
-    return calculate
-}
-console.log(objt.calculateYear());
+//     var currentYear = new Date().getMonth()
+//     var calculate = MYBOD - currentYear
+//     return calculate
+// }
+// console.log(objt.calculateYear());
 
-var userInfo= {
-    name : "faiz",
+// var userInfo= {
+//     name : "faiz",
    
+// }
+// userInfo.show =function(){
+//     var num1 = true;
+//     var num12 = !false;
+//     var result= num1 ==num12
+//     return result
+// }
+// console.log(userInfo.show());
+
+
+
+// var info = {
+//     name:"faiz",
+//     address:"karachi",
+// }
+// info.age= function(){
+//     var currentYear = new Date().getFullYear()
+//     var userDOB = new Date("12-25-1999");
+// var calculation = currentYear-userDOB.getFullYear()
+// return calculation
+// }
+// console.log("user age =>",info.age(),info)
+var info = {
+    name :"john",
+    id:"74684246",
+
 }
-userInfo.show =function(){
-    var num1 = true;
-    var num12 = !false;
-    var result= num1 ==num12
-    return result
-}
-console.log(userInfo.show());
-
-
-
-
-
-
+delete info.name
+info.naming ="faiz"
+console.log(info);
